32aa3a68cd45d4599cc9cccde6179c19befc7477
manuel4life <allotey4eva@gmail.com>
2022-10-18 08:21:57 +0000

    tokenize_getline.c

---
+++ tokenize_getline.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * tokenize - breaks down lines of code in file
+ * into tokens
+ * @line_input: string for line in file
+ * Return: tokens
+ */
+char **tokenize(char *line_input)
+{
+        int bufsize = 1024, i = 0;
+        char *str = malloc(sizeof(char) * strlen(line_input));
+        char *delim = "  $\t\r\n\a";
+        char *token;
+        char **tokens = malloc(sizeof(char *) * bufsize);
+
+        strcpy(str, line_input);
+        token = strtok(str, delim);
+
+        while (token != NULL)
+        {
+                tokens[i] = token;
+                token = strtok(NULL, delim);
+                i++;
+        }
+        return (tokens);
+}
